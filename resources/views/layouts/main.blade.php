ba706d1eaceb70c2f0315a9a439d61520b4060c1
vagrant <vagrant@homestead>
2016-08-07 22:48:17 +0000

    Fixing heroku settings

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,8 +12,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
-    <link rel="stylesheet" type="text/css" href="{{url('css/app.css')}}" />
-    <link rel="stylesheet" type="text/css" href="{{url('css/dashboard.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/dashboard.css')}}" />
 
     @yield('scripts_header')
 
